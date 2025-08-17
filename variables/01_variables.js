dff8f33f4c9a8ac5492faf8b6cde10524e42545a
abuhurairadayan <hurairakhan1030@gmail.com>
2025-08-16 23:34:13 -0700

    getting started with js

---
+++ variables/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 1234;
+let accountEmail = "dayanhuraira@gmail.com";
+var accountPassword = "4321";
+
+accountCity = "Peshawar"
+
+
+// accountId = 2;
+
+accountEmail = "huraira@gmail.com";
+
+accountPassword = "1234";
+
+accountCity = "lahore"
+
+// console.log(accountPassword);
+
+// console.log(accountEmail);
+
+console.table([accountId , accountEmail, accountPassword, accountCity]);
