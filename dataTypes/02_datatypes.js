07e473020618b9b99b09a28e95384b2a60f1b786
abuhurairadayan <hurairakhan1030@gmail.com>
2025-08-19 12:40:34 +0500

    datatypes

---
+++ dataTypes/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // Treat all js code as new version
+
+// alert(3 + 3) //we are using nodejs, not the browser it will give error in node js
+
+let name = "huraira"      // string-datatype
+let age = 23             // number-datatype
+let isLoggedIn = false   // boolean-datatype
+let city;                // undefined
+let country = null       // null
+
+// Datatypes in javaScript
+  // Primitive datatypes
+    // number => 2 to pwer of 53
+    // bigint
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined => 
+    // symbol => uniqueness
+
+  // Non-Primitive datatypes
+    // Object
