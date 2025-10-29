5b6a420a487ae8d4261f22ef706afe9bb352465e
abuhurairadayan <hurairakhan1030@gmail.com>
2025-10-29 08:55:09 +0500

    the understanding of variable keyword var, let and const

--- variables/01_variables.js
+++ variables/01_variables.js
@@ -15,14 +15,86 @@
 
 // use of "var" , "let" and const
 
-var a = 12;
-console.log(a); 
+// var a = 12;
+// console.log(a);           // By using the var keyword it can be redeclared as well as assigned again with a new value.
 
-a = 20;
-console.log(a);
+// a = 20;
+// console.log(a);
 
-var a = 15;
-console.log(a);
+// var a = 15;
+// console.log(a);
+
+
+// {
+       // var a = 5;            // var is a global scope variable it can be read from any where
+//     console.log(a);
+    
+// }
+
+// console.log(a);
+
+
+// use of let keyword as a variable
+
+// let name = "huraira";
+// console.log(name);
+
+// // let name = huraira;  // can't be redeclared
+
+// name = "Dayan";   // can be reassigned with a new value
+// console.log(name);
+
+//scope
+
+// let age = 23;             // let is a block scope variable
+// console.log(age);
+
+// {
+//     console.log(age);
+    
+// }
+
+
+
+// {
+//     let age = 23;
+//     console.log(age);
+    
+// }
+
+// console.log(age);
+
+
+// use of const variable
+
+// const pi = 3.14;     // const variable is used for a fixed or constant value that don't to be change
+// console.log(pi);
+
+// // const pi = 2;        // const can't be redeclared
+
+// pi = 23;                // const can't be re-assigned with a new value
+// console.log(pi);
+
+
+
+{
+    const price = 999;           // const is block scope variable
+    console.log(price);             
+}
+
+console.log(price);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
